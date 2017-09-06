7421b6379b90224b17897cc6703b1d8fb04721b3
Zhuang, Rong <jojozhuang@gmail.com>
2017-09-05 23:41:15 -0700

    resize

--- Game2048/main.js
+++ Game2048/main.js
@@ -1,11 +1,19 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, MenuItem} = require('electron')
 const url = require('url')
 const path = require('path')
 
 let win
 
 function createWindow() {
-   win = new BrowserWindow({width: 530, height: 740})
+   win = new BrowserWindow({//titleBarStyle: 'hidden',
+        width: 530, 
+        height: 740, 
+        minWidth: 530,
+        minHeight: 740,
+        resizable: false,
+        //backgroundColor: '#312450',
+        //show: false,
+        icon: path.join(__dirname, 'resources/ICON175X175.ico') })
    win.loadURL(url.format ({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
@@ -13,4 +21,94 @@ function createWindow() {
    }))
 }
 
-app.on('ready', createWindow) 
+const template = [
+	{
+       label: 'About',
+       submenu: [
+          {
+             role: 'undo'
+          }
+       ]
+    },
+
+   {
+      label: 'Editaaaaa',
+      submenu: [
+         {
+            role: 'undo'
+         },
+         {
+            role: 'redo'
+         },
+         {
+            type: 'separator'
+         },
+         {
+            role: 'cut'
+         },
+         {
+            role: 'copy'
+         },
+         {
+            role: 'paste'
+         }
+      ]
+   },
+
+   {
+      label: 'View',
+      submenu: [
+         {
+            role: 'reload'
+         },
+         {
+            role: 'toggledevtools'
+         },
+         {
+            type: 'separator'
+         },
+         {
+            role: 'resetzoom'
+         },
+         {
+            role: 'zoomin'
+         },
+         {
+            role: 'zoomout'
+         },
+         {
+            type: 'separator'
+         },
+         {
+            role: 'togglefullscreen'
+         }
+      ]
+   },
+
+   {
+      role: 'window',
+      submenu: [
+         {
+            role: 'minimize'
+         },
+         {
+            role: 'close'
+         }
+      ]
+   },
+
+   {
+      role: 'help',
+      submenu: [
+         {
+            label: 'Learn More'
+         }
+      ]
+   }
+]
+
+app.on('ready', () => {
+	const menu = Menu.buildFromTemplate(template)
+	Menu.setApplicationMenu(menu)
+    createWindow()
+})
