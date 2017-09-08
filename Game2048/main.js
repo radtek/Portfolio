fcbc8c4bc24cae4c544ee5ff005597ac26f55526
Zhuang, Rong <jojozhuang@gmail.com>
2017-09-07 22:34:29 -0700

    game2048

--- Game2048/main.js
+++ Game2048/main.js
@@ -1,8 +1,13 @@
 const {app, BrowserWindow, Menu, MenuItem} = require('electron')
 const url = require('url')
 const path = require('path')
+const config = require('./package.json')
 
 let win
+
+app.on('ready', () => {	
+    createWindow()
+})
 
 function createWindow() {
    win = new BrowserWindow({//titleBarStyle: 'hidden',
@@ -11,104 +16,71 @@ function createWindow() {
         minWidth: 530,
         minHeight: 740,
         resizable: false,
+        title: config.windowtitle,
         //backgroundColor: '#312450',
         //show: false,
-        icon: path.join(__dirname, 'resources/ICON175X175.ico') })
+        icon: path.join(__dirname, 'build/icon.icns') })
+   //win.setTitle(require('./package.json').name);
    win.loadURL(url.format ({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
       slashes: true
    }))
+   setMainMenu();
 }
 
-const template = [
-	{
-       label: 'About',
-       submenu: [
-          {
-             role: 'undo'
-          }
-       ]
-    },
-
-   {
-      label: 'Editaaaaa',
-      submenu: [
-         {
-            role: 'undo'
-         },
-         {
-            role: 'redo'
-         },
-         {
-            type: 'separator'
-         },
-         {
-            role: 'cut'
-         },
-         {
-            role: 'copy'
-         },
-         {
-            role: 'paste'
-         }
-      ]
-   },
-
-   {
-      label: 'View',
-      submenu: [
-         {
-            role: 'reload'
-         },
-         {
-            role: 'toggledevtools'
-         },
-         {
-            type: 'separator'
-         },
-         {
-            role: 'resetzoom'
-         },
-         {
-            role: 'zoomin'
-         },
-         {
-            role: 'zoomout'
-         },
-         {
-            type: 'separator'
-         },
-         {
-            role: 'togglefullscreen'
-         }
-      ]
-   },
-
-   {
-      role: 'window',
-      submenu: [
-         {
-            role: 'minimize'
-         },
-         {
-            role: 'close'
-         }
-      ]
-   },
-
-   {
-      role: 'help',
-      submenu: [
-         {
-            label: 'Learn More'
-         }
-      ]
-   }
-]
-
-app.on('ready', () => {
-	const menu = Menu.buildFromTemplate(template)
-	Menu.setApplicationMenu(menu)
-    createWindow()
-})
+function setMainMenu() {
+    const template = [
+        {
+            label: '2048',
+            submenu: [
+                {
+                    role: 'undo'
+                }
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                {
+                    role: 'reload'
+                },
+                {
+                    role: 'toggledevtools'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'resetzoom'
+                },
+                {
+                    type: 'separator'
+                }
+            ]
+        },
+        {
+            role: 'window',
+            submenu: [
+                {
+                    role: 'minimize'
+                },
+                {
+                    role: 'close'
+                }
+            ]
+        },
+        {
+            label: 'Help',
+            role: 'help',
+            submenu: [{
+                label: 'Learn More',
+                click: function () {
+                    electron.shell.openExternal('http://jojozhuang.github.io/blog/2016/11/03/build-cross-platform-apps-with-electron/')
+                }
+            }]
+       }
+    ];
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+}
