ccd367c8e5656b23c217703212e38f33e0b6d3c1
Zhuang, Rong <jojozhuang@gmail.com>
2018-01-02 22:07:04 -0800

    refine course player socket io

--- CoursePlayerSocketIO/test.js
+++
@@ -1,6 +0,0 @@
-var colds = require('./colds');
-colds.getImageData(1080, showdata);
-//console.log(showdata);
-function showdata(json) {
-  console.log('json.length:'+ json.length);
-}
--- CoursePlayerSocketIO/util/constants.js
+++
@@ -1,2 +0,0 @@
-var MAX_ROW_NO = 8;
-var MAX_COL_NO = 8;
