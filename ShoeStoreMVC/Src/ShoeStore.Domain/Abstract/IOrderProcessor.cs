6bf643aa454f756123a9f2ca851095b69f3c2559
Zhuang, Rong <jojozhuang@gmail.com>
2018-07-30 13:05:30 -0700

    delete ShoeStoreMVC

--- ShoeStoreMVC/Src/ShoeStore.Domain/Abstract/IOrderProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Johnny.ShoeStore.Domain.Entities;
-
-namespace Johnny.ShoeStore.Domain.Abstract
-{
-    public interface IOrderProcessor
-    {
-        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
-    }
-}
--- ShoeStoreMVC/Src/ShoeStore.WebUI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Johnny.ShoeStore.WebUI.MvcApplication" Language="C#" %>
